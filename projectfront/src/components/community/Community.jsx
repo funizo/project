8249ce981380ae6ee4dbfcd4eb6b552948d98551
김종현 <plmij1501@gmail.com>
2024-03-07 22:37:32 +0900

    style/#5/community
    -기본 틀 적용

--- projectfront/src/components/community/Community.jsx
+++ projectfront/src/components/community/Community.jsx
@@ -6,7 +6,7 @@ export default function Community() {
             <div className={styles.Banner}>
                 <h2>커뮤니티</h2>
                 <h4>다양한 사람을 만나고 생각의 폭을 넓혀보세요.</h4>
-                <img />
+                {/* <img /> */}
             </div>
             <div className={styles.PostCategory}>
                 <button>작성하기</button>
@@ -17,14 +17,15 @@ export default function Community() {
             </div>
             <div className={styles.PostControl}>
                 <button>리프레시버튼</button>
-                <input>커뮤니티 내 게시글 검색</input>
+                <p>커뮤니티 내 게시글 검색</p>
+                <input placeholder='커뮤니티게시글검색'/>
                 <button>앞페이지 이동</button>
                 <p>페이지(1/1234)</p>
                 <button>뒷페이지 이동</button>
             </div>
             <div className={styles.PostSection}>
                 <div>
-                    <img>아이콘</img>
+                    {/* <img>아이콘</img> */}
                     <p>닉네임</p>
                     <p>게시시 시간</p>
                     <p>제목</p>
